e442de3438ba0c4ce1e9f9bbdf0752736875e6ac
Vishwas R <vishwas.r@fenopix.com>
2019-02-18 10:10:52 +0530

    Improved positioning of export button

--- src/CanvasJS As CSV.js
+++ src/CanvasJS As CSV.js
@@ -23,7 +23,7 @@ function CanvasJSDataAsCSV(chart, fileName) {
 		var base64Img = "data:image/svg+xml;utf8;base64,iVBORw0KGgoAAAANSUhEUgAAABkAAAAZCAYAAADE6YVjAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAGzSURBVEhL7dXPSxRhHMfxqQhEOhVEepK69A/UqZNdAn/gwZOnUAqC8GDHLnpKugQRQmB48+dFJIiEDhbRRQXP4SFEQYRQ0BQl6v3e2Yd9dtl2d5YVPfSBF/vMw/PMd+eZmWeS/2lE7uMr1qt4gbpyA/vYxGwFx/iDl8icB3Cyv5WyixPUVagDTryXO/p3LDKNKWQulKXIB9zFMjIVylLEcaV8aKqm1iIP8TjiVTjP+VVTa5HSOP58FbmNUbTljmqP453n/LPJTXzED7zGZZhrmMEGPsFxxhv+E7fwNN++jor5DLcRT+SaDsG8wW/M4S0GYHwvHNeDCWwhjjtFX9osxK1hPG0m/WhPm8l7bONq7qiQJjjnOXwJFxDHea5KUfxXY2mzKL3wSnbwCBcQsgqv8BeG7YiSqYi5g29wzCs78nmHPdjfZUeUskXcsl1b/+kzdCKO/UvwAQh5AguoxY4oZYss4ggrcJInMBbzuzGPQ8RrH25+uOndCN8Z+w6i40EkrXDLXsMILsE0wyfM/kk4LsSb/x0um7kI2+Hqgi+4goaltFDDC4SEQqdWIMRCLnU+SfIXmIGI6vRCbY8AAAAASUVORK5CYII=";
 		var exportButton = document.createElement('button');
 		var chartBound = chart.container.getBoundingClientRect();
-		exportButton.style.cssText = "position:absolute;display: inline-block;padding: 0px 4px;height: 27px;cursor: pointer;text-align: center;text-decoration: none;background-color: #fff;border: 1px solid rgb(33, 150, 243);left:" + (chartBound.right - (chart.options.zoomEnabled ? 115 : 60)) + "px; top:" + (chartBound.top + 1) + "px";
+		exportButton.style.cssText = "position:absolute;display: inline-block;padding: 0px 4px;height: 27px;cursor: pointer;text-align: center;text-decoration: none;background-color: #fff;border: 1px solid rgb(33, 150, 243);left:" + (chartBound.right - (chart.options.zoomEnabled ? 115 : 60)) + "px; top:" + (chartBound.top + 2) + "px";
 		
 		var img = document.createElement("IMG");
 		img.setAttribute("src", base64Img);
